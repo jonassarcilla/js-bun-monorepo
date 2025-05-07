f2bdb2ccc5d285c298ae85b6a937f6f0f5f8fc8f
Jonas Rafael Sarcilla <jr.sarcilla07@gmail.com>
2025-05-07 19:52:57 +0800

    vite

--- packages/ui/react/vite.config.ts
+++ packages/ui/react/vite.config.ts
@@ -1,14 +1,48 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from "vite-plugin-dts";
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    dts({
+      insertTypesEntry: true,
+      tsconfigPath: './tsconfig.app.json',
+      exclude: ["**/*.stories.ts", "**/*.test.tsx"]
+    }),
   ],
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'ui',
+      fileName: (format) => `ui.${format}.js`,
+      formats: ['es', 'cjs', 'umd'],
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+        "react/jsx-runtime"
+      ],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          "react/jsx-runtime": "react/jsx-runtime"
+        }
+      }
+    }
+  },
   optimizeDeps: {
-    exclude: ["sb-original/image-context"]
-  }
+    exclude: ["sb-original/image-context"],
+    include: ["react/jsx-runtime"]
+  },
+  resolve: {
+    alias: {
+      'react/jsx-runtime': 'react/jsx-runtime.js',
+    },
+  },
 })
